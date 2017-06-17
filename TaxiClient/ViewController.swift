99e5ca341bfb36c71204024e9e26232edfe29cb6
Igor <Igor@iMac-Igor.local>
2017-06-17 23:45:59 +0300

    Initial Commit

---
+++ TaxiClient/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TaxiClient
+//
+//  Created by Igor on 17.06.17.
+//  Copyright © 2017 Igor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
